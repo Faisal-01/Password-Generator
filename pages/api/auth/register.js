67029ae49b8a386df8890fb06f4add3b7de9e2a1
Faisal Iqbal <faisaliqbal012@gmail.com>
2023-06-25 10:05:58 +0500

    bug fixed

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -13,12 +13,14 @@ export default async function handler(req, res) {
         const user = await User.create(req.body);
         res.status(200).json({ success: true, message: "User created successfully" });
       } catch (error) {
-        res.status(400).json({ success: false });
+        res
+          .status(400)
+          .json({ success: false, message: "User not created" });
       }
       break;
 
     default:
-      res.status(400).json({ success: false });
+      res.status(500).json({ success: false, message: "Something went wrong" });
       break;
   }
 }
